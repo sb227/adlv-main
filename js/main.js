acc21dbb49e69732922dacaa1f328832ce8a09de
sb227 <69783498+sb227@users.noreply.github.com>
2021-02-19 13:09:48 +0900

    210219

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 /// 메인 페이지 JS - main.js ////
+var mob = 0; //0-모바일 아님, 1-모바일, 2- 태블릿
+if ($(window).width() <= 768) {
+    mob = 1;
+} //// if ///////////
+
 $(function (e) { ///////////////// JQB //////////////////////
+
+  
+  if(mob){
+    $('.footer_sns').find('p').show();
+  } /////////// 모바일
+
+
+
   console.log('실행');
 
   var swiper = new Swiper('.swiper1', {
@@ -56,5 +69,15 @@ $(function (e) { ///////////////// JQB //////////////////////
     $('.notice_wrap').toggleClass('active');
     $('.wrap').toggleClass('active');
   }); //////// click ////////////////
+
+  $('.footer_sns').find('p').hide();
+  $('.footer_sns li').mouseover(function(){
+    $(this).find('p').stop().fadeIn();
+  }); ////////////// mouseover ///////////
+
+  $('.footer_sns li').mouseleave(function(){
+    $(this).find('p').stop().fadeOut();
+  }); ////////////// mouseleave ///////////
+
 
 }); ///////////////// JQB //////////////////////
